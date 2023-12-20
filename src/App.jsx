d6f88bd3979de959b80a3728bcfd4246b2d9c013
Alimisolution <alimirosheed5000@gmail.com>
2023-12-20 06:32:01 -0800

    client

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import LastestStyles from "./components/LatestStyles";
-import Testimonial from "./components/Testimonial";
 function App() {
   return (
     <div>
-      <Header />
-      <Hero />
-      <About />
-      <LastestStyles />
-      <Testimonial />
-      <Contact />
-      <Footer />
+      <h1>App</h1>
     </div>
   );
 }
